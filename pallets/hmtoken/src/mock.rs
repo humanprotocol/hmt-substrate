adb032073f5757c85c7018045501e8d556abedbf
Alexander Popiak <alexander.popiak@parity.io>
2020-11-12 17:31:11 +0100

    add unit tests

--- pallets/hmtoken/src/mock.rs
+++ pallets/hmtoken/src/mock.rs
@@ -1,57 +1,98 @@
-#[cfg(test)]
-mod tests {
-	use super::*;
+use crate::{Module, Trait};
+use frame_support::{impl_outer_event, impl_outer_origin, parameter_types, weights::Weight};
+use frame_system as system;
+use sp_core::H256;
+use sp_runtime::{
+    testing::Header,
+    traits::{BlakeTwo256, IdentityLookup},
+    Perbill,
+};
 
-	use frame_support::{impl_outer_origin, assert_ok, assert_noop, parameter_types, weights::Weight};
-	use sp_core::H256;
-	use sp_runtime::{Perbill, traits::{BlakeTwo256, IdentityLookup}, testing::Header};
+impl_outer_origin! {
+    pub enum Origin for Test {}
+}
 
-	impl_outer_origin! {
-		pub enum Origin for Test where system = frame_system {}
-	}
+// Configure a mock runtime to test the pallet.
 
-	#[derive(Clone, Eq, PartialEq)]
-	pub struct Test;
-	parameter_types! {
-		pub const BlockHashCount: u64 = 250;
-		pub const MaximumBlockWeight: Weight = 1024;
-		pub const MaximumBlockLength: u32 = 2 * 1024;
-		pub const AvailableBlockRatio: Perbill = Perbill::one();
-	}
-	impl frame_system::Trait for Test {
-		type BaseCallFilter = ();
-		type Origin = Origin;
-		type Index = u64;
-		type Call = ();
-		type BlockNumber = u64;
-		type Hash = H256;
-		type Hashing = BlakeTwo256;
-		type AccountId = u64;
-		type Lookup = IdentityLookup<Self::AccountId>;
-		type Header = Header;
-		type Event = ();
-		type BlockHashCount = BlockHashCount;
-		type MaximumBlockWeight = MaximumBlockWeight;
-		type DbWeight = ();
-		type BlockExecutionWeight = ();
-		type ExtrinsicBaseWeight = ();
-		type MaximumExtrinsicWeight = MaximumBlockWeight;
-		type AvailableBlockRatio = AvailableBlockRatio;
-		type MaximumBlockLength = MaximumBlockLength;
-		type Version = ();
-		type PalletInfo = ();
-		type AccountData = ();
-		type OnNewAccount = ();
-		type OnKilledAccount = ();
-		type SystemWeightInfo = ();
-	}
-	impl Trait for Test {
-		type Event = ();
-		type Balance = u64;
-		type AssetId = u32;
-	}
-	type Assets = Module<Test>;
+#[derive(Clone, Eq, PartialEq)]
+pub struct Test;
+parameter_types! {
+    pub const BlockHashCount: u64 = 250;
+    pub const MaximumBlockWeight: Weight = 1024;
+    pub const MaximumBlockLength: u32 = 2 * 1024;
+    pub const AvailableBlockRatio: Perbill = Perbill::from_percent(75);
+}
 
-	fn new_test_ext() -> sp_io::TestExternalities {
-		frame_system::GenesisConfig::default().build_storage::<Test>().unwrap().into()
-	}
+mod HMTokenPallet {
+    pub use crate::Event;
+}
+
+impl_outer_event! {
+    pub enum TestEvent for Test {
+        HMTokenPallet<T>,
+        system<T>,
+    }
+}
+
+impl system::Trait for Test {
+    type BaseCallFilter = ();
+    type Origin = Origin;
+    type Call = ();
+    type Index = u64;
+    type BlockNumber = u64;
+    type Hash = H256;
+    type Hashing = BlakeTwo256;
+    type AccountId = u64;
+    type Lookup = IdentityLookup<Self::AccountId>;
+    type Header = Header;
+    type Event = TestEvent;
+    type BlockHashCount = BlockHashCount;
+    type MaximumBlockWeight = MaximumBlockWeight;
+    type DbWeight = ();
+    type BlockExecutionWeight = ();
+    type ExtrinsicBaseWeight = ();
+    type MaximumExtrinsicWeight = MaximumBlockWeight;
+    type MaximumBlockLength = MaximumBlockLength;
+    type AvailableBlockRatio = AvailableBlockRatio;
+    type Version = ();
+    type PalletInfo = ();
+    type AccountData = ();
+    type OnNewAccount = ();
+    type OnKilledAccount = ();
+    type SystemWeightInfo = ();
+}
+
+parameter_types! {
+    pub const BulkAccountsLimit: usize = 10;
+    pub const BulkBalanceLimit: u128 = 999;
+
+}
+
+impl Trait for Test {
+    type Event = TestEvent;
+    type Balance = u128;
+    type BulkAccountsLimit = BulkAccountsLimit;
+    type BulkBalanceLimit = BulkBalanceLimit;
+}
+
+pub type HMToken = Module<Test>;
+pub type System = system::Module<Test>;
+
+// Build genesis storage according to the mock runtime.
+pub fn new_test_ext() -> sp_io::TestExternalities {
+    let mut storage = system::GenesisConfig::default()
+        .build_storage::<Test>()
+        .unwrap();
+    crate::GenesisConfig::<Test> {
+        total_supply: 1_000,
+        name: b"Human Protocol Token".to_vec(),
+        symbol: b"HMT".to_vec(),
+        decimals: 15, // ignored in the UI for now
+        initial_account: 1,
+    }
+    .assimilate_storage(&mut storage)
+    .unwrap();
+    let mut ext = sp_io::TestExternalities::from(storage);
+    ext.execute_with(|| System::set_block_number(1));
+    ext
+}
--- pallets/hmtoken/src/tests.rs
+++ pallets/hmtoken/src/tests.rs
@@ -1,88 +1,157 @@
-use crate::{Error, mock::*, RawEvent};
-use frame_support::{assert_ok, assert_noop};
+use crate::{mock::*, Error, RawEvent};
+use frame_support::{assert_noop, assert_ok};
+
+fn last_event() -> TestEvent {
+    frame_system::Module::<Test>::events()
+        .pop()
+        .expect("Event expected")
+        .event
+}
+
 #[test]
-	fn issuing_asset_units_to_issuer_should_work() {
-		new_test_ext().execute_with(|| {
-			assert_ok!(Assets::issue(Origin::signed(1), 100));
-			assert_eq!(Assets::balance(0, 1), 100);
-		});
-	}
+fn config_values_are_initiated() {
+    new_test_ext().execute_with(|| {
+        assert_eq!(HMToken::total_supply(), 1_000);
+        assert_eq!(HMToken::decimals(), 15);
+        assert_eq!(HMToken::name(), b"Human Protocol Token".to_vec());
+        assert_eq!(HMToken::symbol(), b"HMT".to_vec());
+        assert_eq!(HMToken::balance(1), HMToken::total_supply());
+    });
+}
 
-	#[test]
-	fn querying_total_supply_should_work() {
-		new_test_ext().execute_with(|| {
-			assert_ok!(Assets::issue(Origin::signed(1), 100));
-			assert_eq!(Assets::balance(0, 1), 100);
-			assert_ok!(Assets::transfer(Origin::signed(1), 0, 2, 50));
-			assert_eq!(Assets::balance(0, 1), 50);
-			assert_eq!(Assets::balance(0, 2), 50);
-			assert_ok!(Assets::transfer(Origin::signed(2), 0, 3, 31));
-			assert_eq!(Assets::balance(0, 1), 50);
-			assert_eq!(Assets::balance(0, 2), 19);
-			assert_eq!(Assets::balance(0, 3), 31);
-			assert_ok!(Assets::destroy(Origin::signed(3), 0));
-			assert_eq!(Assets::total_supply(0), 69);
-		});
-	}
+#[test]
+fn transfer_passes() {
+    new_test_ext().execute_with(|| {
+        let amount_to_transfer: u128 = 10;
+        let new_balance = HMToken::total_supply() - amount_to_transfer;
+        let from = 1;
+        let to = 2;
+        assert_ok!(HMToken::transfer(
+            Origin::signed(from),
+            to,
+            amount_to_transfer
+        ));
+        assert_eq!(HMToken::balance(from), new_balance);
+        assert_eq!(HMToken::balance(to), amount_to_transfer);
+        assert_eq!(
+            last_event(),
+            TestEvent::HMTokenPallet(RawEvent::Transferred(from, to, amount_to_transfer))
+        );
+    });
+}
 
-	#[test]
-	fn transferring_amount_above_available_balance_should_work() {
-		new_test_ext().execute_with(|| {
-			assert_ok!(Assets::issue(Origin::signed(1), 100));
-			assert_eq!(Assets::balance(0, 1), 100);
-			assert_ok!(Assets::transfer(Origin::signed(1), 0, 2, 50));
-			assert_eq!(Assets::balance(0, 1), 50);
-			assert_eq!(Assets::balance(0, 2), 50);
-		});
-	}
+#[test]
+fn transfer_fails() {
+    new_test_ext().execute_with(|| {
+        assert_noop!(
+            HMToken::transfer(Origin::signed(2), 3, 10),
+            Error::<Test>::BalanceLow
+        );
+        assert_noop!(
+            HMToken::transfer(Origin::signed(1), 2, 0),
+            Error::<Test>::AmountZero
+        );
+    })
+}
 
-	#[test]
-	fn transferring_amount_more_than_available_balance_should_not_work() {
-		new_test_ext().execute_with(|| {
-			assert_ok!(Assets::issue(Origin::signed(1), 100));
-			assert_eq!(Assets::balance(0, 1), 100);
-			assert_ok!(Assets::transfer(Origin::signed(1), 0, 2, 50));
-			assert_eq!(Assets::balance(0, 1), 50);
-			assert_eq!(Assets::balance(0, 2), 50);
-			assert_ok!(Assets::destroy(Origin::signed(1), 0));
-			assert_eq!(Assets::balance(0, 1), 0);
-			assert_noop!(Assets::transfer(Origin::signed(1), 0, 1, 50), Error::<Test>::BalanceLow);
-		});
-	}
+#[test]
+fn bulk_transfer_works() {
+    new_test_ext().execute_with(|| {
+        let amount: u128 = 10;
+        let new_balance = HMToken::total_supply() - amount * 2;
+        let from = 1;
+        let first_rec = 2;
+        let second_rec = 3;
+        let id = 42;
+        assert_ok!(HMToken::transfer_bulk(
+            Origin::signed(from),
+            vec![first_rec, second_rec],
+            vec![amount, amount],
+            id
+        ));
+        assert_eq!(HMToken::balance(from), new_balance);
+        assert_eq!(HMToken::balance(first_rec), amount);
+        assert_eq!(HMToken::balance(second_rec), amount);
+        assert_eq!(
+            last_event(),
+            TestEvent::HMTokenPallet(RawEvent::BulkTransfer(id, 2, 0))
+        );
+    });
+}
 
-	#[test]
-	fn transferring_less_than_one_unit_should_not_work() {
-		new_test_ext().execute_with(|| {
-			assert_ok!(Assets::issue(Origin::signed(1), 100));
-			assert_eq!(Assets::balance(0, 1), 100);
-			assert_noop!(Assets::transfer(Origin::signed(1), 0, 2, 0), Error::<Test>::AmountZero);
-		});
-	}
+#[test]
+fn bulk_transfer_fails_and_passes() {
+    new_test_ext().execute_with(|| {
+        let amount: u128 = HMToken::total_supply() - 10;
+        let new_balance = 0;
+        let from = 1;
+        let first_rec = 2;
+        let second_rec = 3;
+        let id = 42;
+        // Transfer some funds away to make sure that one of the bulk transfers will fail.
+        assert_ok!(HMToken::transfer(Origin::signed(from), 5, 10));
+        assert_ok!(HMToken::transfer_bulk(
+            Origin::signed(from),
+            vec![first_rec, second_rec],
+            vec![amount, 1],
+            id
+        ));
+        assert_eq!(HMToken::balance(from), new_balance);
+        assert_eq!(HMToken::balance(first_rec), amount);
+        assert_eq!(HMToken::balance(second_rec), 0);
 
-	#[test]
-	fn transferring_more_units_than_total_supply_should_not_work() {
-		new_test_ext().execute_with(|| {
-			assert_ok!(Assets::issue(Origin::signed(1), 100));
-			assert_eq!(Assets::balance(0, 1), 100);
-			assert_noop!(Assets::transfer(Origin::signed(1), 0, 2, 101), Error::<Test>::BalanceLow);
-		});
-	}
+        assert_eq!(
+            last_event(),
+            TestEvent::HMTokenPallet(RawEvent::BulkTransfer(id, 1, 1))
+        );
+    });
+}
 
-	#[test]
-	fn destroying_asset_balance_with_positive_balance_should_work() {
-		new_test_ext().execute_with(|| {
-			assert_ok!(Assets::issue(Origin::signed(1), 100));
-			assert_eq!(Assets::balance(0, 1), 100);
-			assert_ok!(Assets::destroy(Origin::signed(1), 0));
-		});
-	}
+#[test]
+fn bulk_transfer_fails() {
+    new_test_ext().execute_with(|| {
+        let amount: u128 = 500;
+        let new_balance = HMToken::total_supply() - amount * 2;
+        let from = 1;
+        let first_rec = 2;
+        let second_rec = 3;
+        let id = 42;
+        assert_noop!(
+            HMToken::transfer_bulk(
+                Origin::signed(from),
+                vec![first_rec],
+                vec![amount, amount],
+                id
+            ),
+            Error::<Test>::MismatchBulkTransfer
+        );
+        assert_noop!(
+            HMToken::transfer_bulk(
+                Origin::signed(from),
+                vec![first_rec, second_rec],
+                vec![amount],
+                id
+            ),
+            Error::<Test>::MismatchBulkTransfer
+        );
 
-	#[test]
-	fn destroying_asset_balance_with_zero_balance_should_not_work() {
-		new_test_ext().execute_with(|| {
-			assert_ok!(Assets::issue(Origin::signed(1), 100));
-			assert_eq!(Assets::balance(0, 2), 0);
-			assert_noop!(Assets::destroy(Origin::signed(2), 0), Error::<Test>::BalanceZero);
-		});
-	}
+        assert_noop!(
+            HMToken::transfer_bulk(
+                Origin::signed(from),
+                vec![first_rec; 11],
+                vec![amount; 11],
+                id
+            ),
+            Error::<Test>::TooManyTos
+        );
+        assert_noop!(
+            HMToken::transfer_bulk(
+                Origin::signed(from),
+                vec![first_rec, second_rec],
+                vec![amount, amount],
+                id
+            ),
+            Error::<Test>::TransferTooBig
+        );
+    });
 }
