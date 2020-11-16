93cd1c695caf64a5e3de294c33c4036e23ddde77
JesseAbram <jesseabramowitz@hotmail.com>
2020-11-16 18:11:36 +0100

    negative tests

--- integrationTests/test/hmToken.test.js
+++ integrationTests/test/hmToken.test.js
@@ -1,3 +1,4 @@
+const should = require('should')
 
 describe('hmToken positive tests', () => {
 	const davePair = {address: "5DAAnrj7VHTznn2AWBemMuyBwZWs6FNFjdyVXUeYum3PTXFy"}
@@ -43,4 +44,93 @@ describe('hmToken positive tests', () => {
 		assert.equal(balanceOfDaveAfter, amountToSend, `dave should have recieved ${amountToSend} tokens`)
 		assert.equal(totalSupply.toString(), totalSupplyAfter.toString(), 'total supply should not have moved')
 	})
+})
+
+describe('hmToken negative tests', () => {
+	it("should fail to transfer balances not enough", async () => {
+		const amountToSend = "10000000000000000000000"
+		try {
+			const tx = halva.polkadot.tx.hmToken.transfer(charliePair.address, amountToSend)
+			await passes(tx, 'Transfer', alicePair);
+			should.fail("tx should have failed but didn't")
+		} catch (e) {
+			assert.equal(
+				e.message,
+				`Transfer : Failed with {"Module":{"index":10,"error":1}}`
+			)
+		}
+	})
+	it("should fail to transfer balances of 0", async () => {
+		const amountToSend = "0"
+		try {
+			const tx = halva.polkadot.tx.hmToken.transfer(charliePair.address, amountToSend)
+			await passes(tx, 'Transfer', alicePair);
+			should.fail("tx should have failed but didn't")
+		} catch (e) {
+			assert.equal(
+				e.message,
+				`Transfer : Failed with {"Module":{"index":10,"error":0}}`
+			)
+		}
+	})
+	it("should fail to transfer bulk to many tos", async () => {
+		const accounts = Array(101).fill(bobPair.address)
+		const amounts = Array(101).fill("10")
+		try {
+			const tx = halva.polkadot.tx.hmToken.transferBulk(accounts, amounts, 11)
+			await passes(tx, 'Bulktransfer', alicePair);
+			should.fail("tx should have failed but didn't")
+		} catch (e) {
+			assert.equal(
+				e.message,
+				`Bulktransfer : Failed with {"Module":{"index":10,"error":4}}`
+			)
+		}
+		
+	})
+	it("should fail to transfer bulk to much value", async () => {
+		const accounts = Array(5).fill(bobPair.address)
+		const amounts = Array(5).fill("1000000000000000000000000000000")
+		try {
+			const tx = halva.polkadot.tx.hmToken.transferBulk(accounts, amounts, 11)
+			await passes(tx, 'Bulktransfer', alicePair);
+			should.fail("tx should have failed but didn't")
+		} catch (e) {
+			assert.equal(
+				e.message,
+				`Bulktransfer : Failed with {"Module":{"index":10,"error":5}}`
+			)
+		}
+	})
+	it("should fail to transfer bulk mismatch accounts and values", async () => {
+		const accounts = Array(5).fill(bobPair.address)
+		const amounts = Array(6).fill("10")
+		try {
+			const tx = halva.polkadot.tx.hmToken.transferBulk(accounts, amounts, 11)
+			await passes(tx, 'Bulktransfer', alicePair);
+			should.fail("tx should have failed but didn't")
+		} catch (e) {
+			assert.equal(
+				e.message,
+				`Bulktransfer : Failed with {"Module":{"index":10,"error":3}}`
+			)
+		}
+
+	})
+	it("should fail silently and still transfer funds to one on a failing batch tx", async () => {
+		const balanceAlice = await halva.polkadot.query.hmToken.balances(alicePair.address)
+		const accounts = [bobPair.address, charliePair.address]
+		const amounts = Array(2).fill(balanceAlice)
+		const balanceOfCharlieBefore = await halva.polkadot.query.hmToken.balances(charliePair.address)
+		const balanceOfBobBefore = await halva.polkadot.query.hmToken.balances(bobPair.address)
+		const tx = halva.polkadot.tx.hmToken.transferBulk(accounts, amounts, 11)
+		await passes(tx, 'Bulktransfer', alicePair);
+		const balanceOfCharlieAfter = await halva.polkadot.query.hmToken.balances(charliePair.address)
+		const balanceOfBobAfter = await halva.polkadot.query.hmToken.balances(bobPair.address)
+		const calculatedBalanceofBob = Number(balanceOfBobBefore) + Number(balanceAlice)
+		assert.equal(Number(balanceOfBobAfter.toString()), calculatedBalanceofBob.toString(), "bob should have gotten paid out")
+		assert.equal(balanceOfCharlieBefore.toString(), balanceOfCharlieAfter.toString(), "charlie should not have gotten paid out")
+
+	})
+
 })
