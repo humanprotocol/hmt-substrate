942fb51838a49bcadabce2553d29a8bdff693ab0
Alexander Popiak <alexander.popiak@parity.io>
2020-11-24 18:17:19 +0100

    add bulk payout benchmark

--- pallets/escrow/src/benchmarks.rs
+++ pallets/escrow/src/benchmarks.rs
@@ -9,6 +9,8 @@ use frame_benchmarking::{account, benchmarks, whitelisted_caller};
 use frame_support::assert_ok;
 use frame_system::{EventRecord, RawOrigin};
 
+pub type BalanceOf<T> = <<T as Trait>::Currency as Currency<<T as frame_system::Trait>::AccountId>>::Balance;
+
 const SEED: u32 = 0;
 
 fn assert_last_event<T: Trait>(generic_event: <T as Trait>::Event) {
@@ -36,6 +38,7 @@ benchmarks! {
 	create {
 		let h in 1..(T::HandlersLimit::get() as u32);
 		let s in 1..(T::StringLimit::get() as u32);
+
 		let caller: T::AccountId = whitelisted_caller();
 		let canceller: T::AccountId = account("canceller", 0, SEED);
 		let handlers: Vec<T::AccountId> = (0..h).map(|h| account("handler", h, SEED)).collect();
@@ -62,6 +65,7 @@ benchmarks! {
 	
 	abort {
 		let h in 1..(T::HandlersLimit::get() as u32);
+
 		let caller: T::AccountId = whitelisted_caller();
 		let canceller: T::AccountId = account("canceller", 0, SEED);
 		let handlers: Vec<T::AccountId> = (0..h).map(|h| account("handler", h, SEED)).collect();
@@ -136,8 +140,9 @@ benchmarks! {
 	}
 
 	store_results {
-		let h = T::HandlersLimit::get() as u32;
 		let s in 1..(T::StringLimit::get() as u32);
+
+		let h = T::HandlersLimit::get() as u32;
 		let caller: T::AccountId = whitelisted_caller();
 		let canceller: T::AccountId = account("canceller", 0, SEED);
 		let handlers: Vec<T::AccountId> = (0..h).map(|h| account("handler", h, SEED)).collect();
@@ -156,6 +161,46 @@ benchmarks! {
 	} : _(RawOrigin::Signed(caller.clone()), id, url.clone(), hash.clone())
 	verify {
 		assert_last_event::<T>(RawEvent::IntermediateStorage(id, url, hash).into())
+	}
+
+	bulk_payout {
+		let s in 1..(T::StringLimit::get() as u32);
+		let b in 1..(T::BulkAccountsLimit::get() as u32);
+
+		let h = T::HandlersLimit::get() as u32;
+		let caller: T::AccountId = whitelisted_caller();
+		let canceller: T::AccountId = account("canceller", 0, SEED);
+		let handlers: Vec<T::AccountId> = (0..h).map(|h| account("handler", h, SEED)).collect();
+		let junk = 42;
+		let manifest_url = vec![junk; T::StringLimit::get()];
+		let manifest_hash = vec![junk; T::StringLimit::get()];
+		let reputation_oracle: T::AccountId = account("oracle", 0, SEED);
+		let recording_oracle: T::AccountId = account("oracle", 1, SEED);
+		let reputation_oracle_stake = Percent::from_percent(10);
+		let recording_oracle_stake = Percent::from_percent(10);
+
+		assert_ok!(Escrow::<T>::create(RawOrigin::Signed(caller.clone()).into(), canceller.clone(), handlers.clone(), manifest_url.clone(), manifest_hash.clone(), reputation_oracle.clone(), recording_oracle.clone(), reputation_oracle_stake, recording_oracle_stake));
+		let id = 0;
+		let results_url = vec![junk; s as usize];
+		let results_hash = vec![junk; s as usize];
+		let tx_id = 0;
+		let escrow = Escrows::<T>::get(id).unwrap();
+		let amount: BalanceOf<T> = 10.into();
+		let total_amount = amount * b.into();
+		T::Currency::make_free_balance_be(&escrow.account, total_amount.into());
+		let recipients: Vec<T::AccountId> = (0..b).map(|b| account("recipient", b, SEED)).collect();
+		let amounts = vec![amount; b as usize];
+	} : _(RawOrigin::Signed(caller.clone()), id, recipients.clone(), amounts.clone(), Some(results_url.clone()), Some(results_hash.clone()), tx_id)
+	verify {
+		assert_eq!(FinalResults::get(id), Some(ResultInfo { results_url, results_hash }));
+		assert_eq!(T::Currency::free_balance(&reputation_oracle), b.into());
+		assert_eq!(T::Currency::free_balance(&recording_oracle), b.into());
+		let received =  amount - reputation_oracle_stake.mul_floor(amount) - recording_oracle_stake.mul_floor(amount);
+		for r in recipients {
+			assert_eq!(T::Currency::free_balance(&r), received);
+		}
+		assert_eq!(Escrows::<T>::get(id).unwrap().status, EscrowStatus::Paid);
+		assert_last_event::<T>(RawEvent::BulkPayout(id, tx_id).into());
 	}
 
 }
@@ -198,6 +243,13 @@ mod tests {
 	fn escrow_store_results() {
 		new_test_ext().execute_with(|| {
 			assert_ok!(test_benchmark_store_results::<Test>());
+		});
+	}
+
+	#[test]
+	fn escrow_bulk_payout() {
+		new_test_ext().execute_with(|| {
+			assert_ok!(test_benchmark_bulk_payout::<Test>());
 		});
 	}
 }
